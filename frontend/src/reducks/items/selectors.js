78b9f50d7664b65fd529695f7f21ed9a1aef4207
johnakhtar <fawaddr@yahoo.com>
2023-03-08 10:02:16 -0800

    backend

--- frontend/src/reducks/items/selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-const itemsSelector = (state) => state.items;
-
-export const getItems = createSelector(
-    [itemsSelector],
-    state => state.list
-);
